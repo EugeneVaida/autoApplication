3d4cbe89397ccb1e9973d95355620b4971389843
Евгений Вайда <e.v.e.r.e.s.tt.1551@gmail.com>
2018-11-05 04:55:40 +0300

    Improvements

--- autoApp/autoApp/Controllers/CarsController.cs
+++ autoApp/autoApp/Controllers/CarsController.cs
@@ -18,76 +18,20 @@ namespace autoApp.Controllers
 {
     public class CarsController : Controller
     {
-        private CarContext db = new CarContext();
+        CarContext db = new CarContext();
 
-        // GET: Cars
-        public ActionResult Index(string price, int? manufacturer, int? model)
-        {
-            IQueryable<Car> cars;
-
-            cars = db.Cars.Include(c => c.Model).Include(c => c.Model.Manufacturer);
-
-            if (manufacturer != null)
-            {
-                cars = cars.Where(x => x.Model.ManufacturerId == manufacturer);
-            }
-            if (model != null)
-            {
-                cars = cars.Where(x => x.ModelId == model);
-            }
-
-
-            if (price == "")
-            {
-                ViewBag.Price = "Low";
-            }
-            else
-            {
-                ViewBag.Price = price;
-            }
-            
-            var eql = (price == "High") ?
-                cars = cars.OrderByDescending(x => x.Price)
-                :
-                cars = cars.OrderBy(x => x.Price);
-
-            ViewBag.Manufacturer = manufacturer;
-            ViewBag.Model = model;
-            ViewBag.ManufacturerId = new SelectList(db.Manufacturers, "Id", "Name", manufacturer);
-            ViewBag.ModelId = new SelectList(db.Models.Where(x => x.ManufacturerId == manufacturer), "Id", "Name", model);
-            return View(cars.ToList());
-        }
-
-        // GET: Cars/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Car car = db.Cars.Find(id);
-            if (car == null)
-            {
-                return HttpNotFound();
-            }
-            return View(car);
-        }
-
-        // GET: Cars/Create
+        #region Create
         public ActionResult Create()
-        {
+        {            
             ViewBag.ManufacturerId = new SelectList(db.Manufacturers, "Id", "Name");
             ViewBag.ModelId = new SelectList(db.Models, "Id", "Name");
-            return View();
+            return View();            
         }
 
-        // POST: Cars/Create
-        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
-        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create(Car car)
-        {
+        {            
             if (ModelState.IsValid)
             {
                 if (Request.Files.Count != 0)
@@ -98,88 +42,138 @@ namespace autoApp.Controllers
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            
-            return View(car);
+            return View(car);            
         }
+        #endregion
 
-        // GET: Cars/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Car car = db.Cars.Find(id);
-            if (car == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.ManufacturerId = new SelectList(db.Manufacturers, "Id", "Name", car.Model.ManufacturerId);
-            ViewBag.ModelId = new SelectList(db.Models.Where(x => x.ManufacturerId == car.Model.ManufacturerId), "Id", "Name", car.ModelId);
-            return View(car);
-        }
+        #region Read
+            public ActionResult Index(string price, int? manufacturer, int? model)
+            {                
+                IQueryable<Car> cars;
 
-        // POST: Cars/Edit/5
-        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
-        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(Car car)
-        {
-            if (ModelState.IsValid)
-            {
-                Car oldCar = db.Cars.Find(car.Id);
-                if (Request.Files.Count != 0)
+                cars = db.Cars.Include(c => c.Model).Include(c => c.Model.Manufacturer);
+
+                if (manufacturer != null)
                 {
-                   car.Image = CreateImageLink(Request.Files[0]);
+                    cars = cars.Where(x => x.Model.ManufacturerId == manufacturer);
                 }
-                oldCar = ToNewCar(oldCar, car);
-                //db.Entry(car).State = EntityState.Modified;
-                
+                if (model != null)
+                {
+                    cars = cars.Where(x => x.ModelId == model);
+                }
+
+
+                if (price == "")
+                {
+                    ViewBag.Price = "Low";
+                }
+                else
+                {
+                    ViewBag.Price = price;
+                }
+
+                var eql = (price == "High") ?
+                    cars = cars.OrderByDescending(x => x.Price)
+                    :
+                    cars = cars.OrderBy(x => x.Price);
+
+                ViewBag.Manufacturer = manufacturer;
+                ViewBag.Model = model;
+                ViewBag.ManufacturerId = new SelectList(db.Manufacturers, "Id", "Name", manufacturer);
+                ViewBag.ModelId = new SelectList(db.Models.Where(x => x.ManufacturerId == manufacturer), "Id", "Name", model);
+                return View(cars.ToList());                
+
+            }
+            public ActionResult Details(int? id)
+            {                
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                Car car = db.Cars.Find(id);
+                if (car == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(car); 
+            }
+        #endregion
+
+        #region Update
+            public ActionResult Edit(int? id)
+            {                
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                Car car = db.Cars.Find(id);
+                if (car == null)
+                {
+                    return HttpNotFound();
+                }
+                ViewBag.ManufacturerId = new SelectList(db.Manufacturers, "Id", "Name", car.Model.ManufacturerId);
+                ViewBag.ModelId = new SelectList(db.Models.Where(x => x.ManufacturerId == car.Model.ManufacturerId), "Id", "Name", car.ModelId);
+                return View(car);                
+
+            }
+
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public ActionResult Edit(Car car)
+            {
+                if (ModelState.IsValid)
+                {
+                    Car oldCar = db.Cars.Find(car.Id);
+                    if (Request.Files.Count != 0)
+                    {
+                        car.Image = CreateImageLink(Request.Files[0]);
+                    }
+                    oldCar = ToNewCar(oldCar, car);
+                    //db.Entry(car).State = EntityState.Modified;
+
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                ViewBag.ManufacturerId = new SelectList(db.Manufacturers, "Id", "Name", car.Model.ManufacturerId);
+                ViewBag.ModelId = new SelectList(db.Models, "Id", "Name", car.ModelId);
+                return View(car); 
+            }
+        #endregion
+
+        #region Delete
+            public ActionResult Delete(int? id)
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                Car car = db.Cars.Find(id);
+                if (car == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(car);
+            }
+
+            [HttpPost, ActionName("Delete")]
+            [ValidateAntiForgeryToken]
+            public ActionResult DeleteConfirmed(int id)
+            {
+                Car car = db.Cars.Find(id);
+                DeleteImage(car);
+                db.Cars.Remove(car);
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("Index"); 
             }
-            ViewBag.ManufacturerId = new SelectList(db.Manufacturers, "Id", "Name", car.Model.ManufacturerId);
-            ViewBag.ModelId = new SelectList(db.Models, "Id", "Name", car.ModelId);
-            return View(car);
-        }
-
-        // GET: Cars/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            protected override void Dispose(bool disposing)
+            {                
+                if (disposing)
+                {
+                    db.Dispose();
+                }
+                base.Dispose(disposing); 
             }
-            Car car = db.Cars.Find(id);
-            if (car == null)
-            {
-                return HttpNotFound();
-            }
-            return View(car);
-        }
-
-        // POST: Cars/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Car car = db.Cars.Find(id);
-            DeleteImage(car);
-            db.Cars.Remove(car);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-
+        #endregion
 
         #region HelperMethods
 
@@ -204,7 +198,7 @@ namespace autoApp.Controllers
 
         [HttpGet]
         public ActionResult GetModelsByManufacturerId(int id)
-        {
+        {            
             var data = db.Models.Where(x => x.ManufacturerId == id).ToList();
             var result = Json(ToModelItemList(data), JsonRequestBehavior.AllowGet);
             return result;
